7ce434e97fb6ac3210eb14076b79dbee17b63b22
Michael Neely <michaelneely@protonmail.com>
2021-05-07 08:59:04 -0700

    add rough gcp setup script

---
+++ gcp_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+virtualenv -p python3 .
+
+source ./bin/activate
+
+pip install t5[gcp]
+
+cd google-research
+
+pip install -r wt5/requirements.txt
+
+python -m spacy download en_core_web_sm
+
+cd ..
+
+pip install ./datasets/
