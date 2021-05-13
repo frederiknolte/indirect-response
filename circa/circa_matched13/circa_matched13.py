315c404a9e7134aaad855dc28ebd4ff8c8d4abd6
yun <liyun.elf@gmail.com>
2021-05-13 12:32:32 +0200

    use relative path

--- circa/circa_matched13/circa_matched13.py
+++ circa/circa_matched13/circa_matched13.py
@@ -2,17 +2,19 @@
 
 import tensorflow_datasets as tfds
 
+import os
 import sys
-sys.path.append('..')
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 from circa_helper import _Circa
 
 
-class CircaMatched13(_Circa,tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for circa_matched_13 dataset."""
+class CircaMatched13(_Circa, tfds.core.GeneratorBasedBuilder):
+    """DatasetBuilder for circa_matched_13 dataset."""
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-  _SETUP = 'matched'
-  _SEED = 13
+    VERSION = tfds.core.Version("1.0.0")
+    RELEASE_NOTES = {
+        "1.0.0": "Initial release.",
+    }
+    _SETUP = "matched"
+    _SEED = 13
