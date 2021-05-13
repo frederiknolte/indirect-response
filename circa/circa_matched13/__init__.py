e67608b9ebad01cb12dad7e41e8a9a784bebe701
yun <liyun.elf@gmail.com>
2021-05-13 12:19:29 +0200

    add circa matched 13

---
+++ circa/circa_matched13/__init__.py
@@ -0,0 +1,3 @@
+"""circa_matched13 dataset."""
+
+from .circa_matched13 import CircaMatched13
---
+++ circa/circa_matched13/circa_matched13.py
@@ -0,0 +1,18 @@
+"""circa_matched13 dataset."""
+
+import tensorflow_datasets as tfds
+
+import sys
+sys.path.append('..')
+from circa_helper import _Circa
+
+
+class CircaUnmatched13(_Circa,tfds.core.GeneratorBasedBuilder):
+  """DatasetBuilder for circa_matched_13 dataset."""
+
+  VERSION = tfds.core.Version('1.0.0')
+  RELEASE_NOTES = {
+      '1.0.0': 'Initial release.',
+  }
+  _SETUP = 'matched'
+  _SEED = 13
