30aee7d2f2d9b341d4a7839035c2c67bd6353e87
yun <liyun.elf@gmail.com>
2021-05-21 10:20:03 +0200

    script for generating inputs for mturk

--- scripts/generate_questions.py
+++ scripts/generate_questions.py
@@ -165,14 +165,90 @@ def gen_googleform_explain_nli(
     return df
 
 
+def gen_mturk_explain_nli(
+    inputs_file: os.PathLike,
+    targets_file: os.PathLike,
+    predictions_file: os.PathLike,
+    use_context: bool = True,
+    strict: bool = True,
+    num_questions: int = 20,
+    tsv_file: os.PathLike = CIRCA,
+    seed: int = 7,
+) -> pd.DataFrame:
+
+    NLI_MAPPING = {
+        "entailment": "Yes",
+        "neutral": "In the middle, neither yes nor no",
+        "contradiction": "No",
+    }
+    np.random.seed(seed)
+
+    df_circa = read_circa(tsv_file)
+    if strict:
+        goldstandard = "goldstandard1"
+    else:
+        goldstandard = "goldstandard2"
+
+    df_circa = df_circa[
+        (df_circa[goldstandard] != "nan") & (df_circa[goldstandard] != "Other")
+    ]
+
+    inputs = list(map(lambda x: eval(x).decode("utf-8"), open(inputs_file).readlines()))
+    targets = list(map(eval, open(targets_file).readlines()))
+    predictions = list(map(eval, open(predictions_file).readlines()))
+
+    df = pd.DataFrame()
+    df["inputs"] = inputs
+    df["targets"] = targets
+    df["predictions"] = predictions
+
+    # process inputs
+    df["inputs"] = df.inputs.apply(lambda x: x.split("hypothesis: ")[1].strip())
+    df["canquestion-X"] = df.inputs.apply(
+        lambda x: x.split("premise:")[0].strip()
+    ).astype("string")
+    df["answer-Y"] = df.inputs.apply(lambda x: x.split("premise:")[1].strip()).astype(
+        "string"
+    )
+    df = (
+        df.set_index(["canquestion-X", "answer-Y"])
+        .join(df_circa.set_index(["canquestion-X", "answer-Y"]), how="inner")
+        .reset_index()
+    )
+
+    # process targets
+    df = df[df.targets.apply(lambda x: x["label"] != "none")]
+    df["target"] = df.targets.apply(lambda x: NLI_MAPPING[x["label"]])
+    # TODO: check this after retraining
+    # assert (df.targets != df.goldstandard1).all()
+
+    # process predictions
+    df["explanation"] = df.predictions.apply(lambda x: x["explanations"][0])
+    df["prediction"] = df.predictions.apply(
+        lambda x: NLI_MAPPING.get(x["label"], "None")
+    )
+
+    # coalesce target and prediction
+    columns = ["context", "question-X", "answer-Y", "explanation"]
+    df_target = df[columns + ["target"]].rename(columns={"target": "interpretation"})
+    df_prediction = df[columns + ["prediction"]].rename(
+        columns={"prediction": "interpretation"}
+    )
+    df = pd.concat([df_target, df_prediction]).drop_duplicates()
+
+    return df.rename(columns={"question-X": "question", "answer-Y": "answer"})
+
+
 if __name__ == "__main__":
-    df = read_circa()
+    # df = read_circa()
     # calc_bleu_scores()
 
-    questions_interpret = gen_googleform_interpret()
-    questions_interpret.to_csv("googleform_interpret.csv", sep="|")
+    # questions_interpret = gen_googleform_interpret()
+    # questions_interpret.to_csv("googleform_interpret.csv", sep="|")
 
-    questions_explain = gen_googleform_explain_nli(
-        "circa_inputs", "circa_targets", "circa_predictions"
-    )
-    questions_explain[['concatenated']].to_csv('googleform_explain.csv')
+    # questions_explain = gen_googleform_explain_nli(
+    #     "circa_inputs", "circa_targets", "circa_predictions"
+    # )
+    # questions_explain[['concatenated']].to_csv('googleform_explain.csv')
+    df = gen_mturk_explain_nli("circa_inputs", "circa_targets", "circa_predictions")
+    df.to_csv("input_explanation.csv", index=False)
