51cfc0f696e7300594b0db577812f02571d81c6c
yun <liyun.elf@gmail.com>
2021-05-26 16:44:44 +0200

    significance test for bleu scores

--- scripts/direct_answers_bleu.py
+++ scripts/direct_answers_bleu.py
@@ -1,8 +1,11 @@
 import os
+from itertools import combinations
 
 import numpy as np
 import pandas as pd
 import sacrebleu
+from scipy.stats import wilcoxon
+from tqdm import tqdm
 
 CIRCA = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "../circa/circa-data.tsv"
@@ -18,30 +21,58 @@ def read_circa(tsv_file: os.PathLike = CIRCA) -> pd.DataFrame:
         else:
             df[col] = df[col].astype("string")
 
+    df = df[~df["canquestion-X"].isna()]
     return df
+
+
+def _calc_bleu(_df):
+
+    bleu = sacrebleu.corpus_bleu(
+        _df["answer-Y"].tolist(), [_df["canquestion-X"].tolist()]
+    )
+    return bleu
 
 
 def calc_bleu_scores(tsv_file=CIRCA):
     df = read_circa(tsv_file)
 
-    bleu = sacrebleu.corpus_bleu(
-        df["answer-Y"].tolist(), [df["canquestion-X"].tolist()]
-    )
-    bleu_gs1 = df.groupby("goldstandard1").apply(
-        lambda _df: sacrebleu.corpus_bleu(
-            _df["answer-Y"].tolist(), [_df["canquestion-X"].tolist()]
-        )
-    )
-    bleu_gs2 = df.groupby("goldstandard2").apply(
-        lambda _df: sacrebleu.corpus_bleu(
-            _df["answer-Y"].tolist(), [_df["canquestion-X"].tolist()]
-        )
-    )
+    bleu = _calc_bleu(df)
+    bleu_gs1 = df.groupby("goldstandard1").apply(_calc_bleu)
+    bleu_gs2 = df.groupby("goldstandard2").apply(_calc_bleu)
 
-    print("bleu:", bleu, "per goldstandard1:", bleu_gs1, "per goldstandard2:", bleu_gs2)
     return bleu, bleu_gs1, bleu_gs2
+
+
+def bleu_significance_test(
+    tsv_file=CIRCA, relaxed=True, num_samples=100, sample_size=100
+):
+    '''wilcoxon test for samples from pairs of categories'''
+
+    df = read_circa(tsv_file)
+    if relaxed:
+        goldstandard = "goldstandard2"
+    else:
+        goldstandard = "goldstandard1"
+    df = df[~df[goldstandard].isna()]
+    labels = df[goldstandard].unique()
+
+    significance = dict()
+    for l1, l2 in tqdm(combinations(labels, 2)):
+        bleu1 = np.zeros(num_samples)
+        bleu2 = np.zeros(num_samples)
+        for ct in range(num_samples):
+            bleu1[ct] = _calc_bleu(
+                df[df[goldstandard] == l1].sample(sample_size, replace=True)
+            ).score
+            bleu2[ct] = _calc_bleu(
+                df[df[goldstandard] == l2].sample(sample_size, replace=True)
+            ).score
+        significance[f"{l1} - {l2}"] = wilcoxon(bleu1, bleu2)
+
+    return significance
 
 
 if __name__ == "__main__":
     df = read_circa()
-    calc_bleu_scores()
+    bleu, bleu_gs1, bleu_gs2 = calc_bleu_scores()
+    significance = bleu_significance_test()
